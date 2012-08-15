141bc8c3d909a5651d48d055f2cc067ca0ed4025
smlg <smlgroot@gmail.com>
2012-08-15 18:45:13 -0500

    box2d mouse joints, game walls

--- Source/com/kalimeradev/test/states/PlayState.hx
+++ Source/com/kalimeradev/test/states/PlayState.hx
@@ -30,6 +30,10 @@ import nme.display.StageAlign;
 import nme.display.StageScaleMode;
 import nme.events.Event;
 import nme.Lib;
+import box2D.collision.B2AABB;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2MouseJoint;
+import box2D.dynamics.joints.B2MouseJointDef;
 ///////////
 class PlayState extends SmState
 {
@@ -64,17 +68,39 @@ class PlayState extends SmState
 			construct ();
 		///////////////
 	}
+	public var mouseJoint:B2MouseJoint;
 	public function onPlayBack(e:MouseEvent):Void {
+		
+		
+		//bodyTest.applyForce(new B2Vec2(-50,-50),bodyTest.getWorldCenter());
+		//trace("force applied");
+		///////
 		/*var menuState:MenuState= new MenuState();
 		SmH.switchState(menuState);*/
 		
-		trace(e.stageY+"+++"+e.localY);
-		createCircle(e.stageX, e.stageY , 10, true);
+		//trace(e.stageY+"+++"+e.localY);
+		//createCircle(e.stageX, e.stageY , 10, true);
 		
 		/*var g:B2Vec2 = World.getGravity();
 		g.y = g.y * -1;
 		
 		World.setGravity(g);*/
+		
+		GetBodyAtMouse(false, e);
+		if (queriedBody!=null) {
+				var mouseJointDef:B2MouseJointDef=new B2MouseJointDef();
+				mouseJointDef.bodyA=World.getGroundBody();
+				mouseJointDef.bodyB=queriedBody;
+				mouseJointDef.target.set(e.stageX*PHYSICS_SCALE, e.stageX*PHYSICS_SCALE);
+				mouseJointDef.maxForce=30000;
+				//mouseJointDef.timeStep=m_timeStep;
+				mouseJointDef.frequencyHz =  30;
+				//mouseJoint =
+				mouseJoint= cast(World.createJoint(mouseJointDef),B2MouseJoint);
+				trace("createJoint");
+		}
+		
+		trace("finish GetBodyAtMouse");
 	}
 	private function initialize ():Void {
 	 
@@ -98,13 +124,16 @@ class PlayState extends SmState
 		//debugDraw.setFlags (B2DebugDraw.e_centerOfMassBit);
 		World.setDebugDraw (debugDraw);
 	 
-		createBox (250, SmH.height, 500, 100, false);
+		//createBox (0, SmH.height,  SmH.width, 100, false);
 		createBox (250, 100, 100, 100, true);
 		createCircle (100, 10, 20, true);
-		bodyTest = createCircle (105, 100, 50, true);
+		bodyTest = createCircle (105, 100, 30, true);
 	 createMap();
+	 createWalls();
 		addEventListener (Event.ENTER_FRAME, this_onEnterFrame);
-		SmH.game.addEventListener(MouseEvent.CLICK, onPlayBack);
+		SmH.game.addEventListener(MouseEvent.MOUSE_DOWN, onPlayBack);
+		SmH.game.addEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);
+		SmH.game.addEventListener(MouseEvent.MOUSE_UP, onMouseDown);
 	 
 	}
 	private function createBox (x:Float, y:Float, width:Float, height:Float, dynamicBody:Bool):B2Body {
@@ -146,8 +175,8 @@ class PlayState extends SmState
 		
 		var fixtureDefinition = new B2FixtureDef ();
 		fixtureDefinition.shape = circle;
-		//fixtureDefinition.restitution = 2;
-		fixtureDefinition.density = 1;
+		fixtureDefinition.restitution = .5;
+		fixtureDefinition.density = .1;
 		fixtureDefinition.friction = .1;
 		
 		var body = World.createBody (bodyDefinition);
@@ -156,37 +185,55 @@ class PlayState extends SmState
 	}
 	
 	private function createMap():Void {
-	 trace("createMap");
+		trace("createMap");
 		var bodyDefinition = new B2BodyDef ();
-		bodyDefinition.position.set (20 * PHYSICS_SCALE, 20 * PHYSICS_SCALE);
+		bodyDefinition.position.set (100 * PHYSICS_SCALE, 100 * PHYSICS_SCALE);
 
-		/*var polygon = new B2PolygonShape ();
-		polygon.setAsBox ((width / 2) * PHYSICS_SCALE, (height / 2) * PHYSICS_SCALE);*/
+		// This an edge shape with ghost vertices.
 
+		var v0:B2Vec2 = new B2Vec2(0* PHYSICS_SCALE, 100* PHYSICS_SCALE);
+
+		var v1:B2Vec2 = new B2Vec2(100* PHYSICS_SCALE, 100* PHYSICS_SCALE);
+
+		var v2:B2Vec2 = new B2Vec2(200* PHYSICS_SCALE, 200* PHYSICS_SCALE);
+
+		var v3:B2Vec2 = new B2Vec2(200* PHYSICS_SCALE, 300* PHYSICS_SCALE);
 		
+		var v4:B2Vec2 = new B2Vec2(0* PHYSICS_SCALE, 100* PHYSICS_SCALE);
+
+		var edge:B2PolygonShape = B2PolygonShape.asArray([v0, v1, v2, v3,v4], 4);
+		var edge2:B2EdgeShape = new B2EdgeShape(v0, v1);
 		
-// This an edge shape with ghost vertices.
+		var fixtureDefinition = new B2FixtureDef ();
+		fixtureDefinition.shape = edge;
+		fixtureDefinition.density = 1;
+		fixtureDefinition.friction = 0.3;
+	 
+		var body:B2Body = World.createBody (bodyDefinition);
+		body.createFixture (fixtureDefinition);
 
-var v0:B2Vec2 = new B2Vec2(0* PHYSICS_SCALE, 100* PHYSICS_SCALE);
-
-var v1:B2Vec2 = new B2Vec2(100* PHYSICS_SCALE, 100* PHYSICS_SCALE);
-
-var v2:B2Vec2 = new B2Vec2(200* PHYSICS_SCALE, 200* PHYSICS_SCALE);
-
-var v3:B2Vec2 = new B2Vec2(0* PHYSICS_SCALE, 100* PHYSICS_SCALE);
-
- 
-var edge:B2PolygonShape = B2PolygonShape.asArray([v0, v1, v2, v3], 3);
-
-/*edge.m_hasVertex0 = true;
-
-edge.m_hasVertex3 = true;
-
-edge.m_vertex0 = v0;
-
-edge.m_vertex3 = v3;*/
+	}
+	
+	private function createWalls():Void {
+		trace("createWalls");
+		 
+		createEdge(0,0,new B2Vec2(0, 0), new B2Vec2(0,SmH.height));//Left.
+		createEdge(0,0,new B2Vec2(0,0), new B2Vec2(SmH.width,0));//Top.
+		createEdge(0,0,new B2Vec2(SmH.width,0), new B2Vec2(SmH.width,SmH.height));//Right.
+		createEdge(0,0,new B2Vec2(SmH.width,SmH.height), new B2Vec2(0,SmH.height));//Bottom.
 
 
+	}
+	private function createEdge(x:Float,y:Float,v1:B2Vec2,v2:B2Vec2):Void {
+		trace("createEdge");
+		var bodyDefinition = new B2BodyDef ();
+		bodyDefinition.position.set (x * PHYSICS_SCALE, y * PHYSICS_SCALE);
+		 
+		v1.set(v1.x * PHYSICS_SCALE, v1.y * PHYSICS_SCALE);
+		v2.set(v2.x * PHYSICS_SCALE, v2.y * PHYSICS_SCALE);
+
+		var edge:B2PolygonShape = B2PolygonShape.asEdge(v1,v2);
+		
 		var fixtureDefinition = new B2FixtureDef ();
 		fixtureDefinition.shape = edge;
 		fixtureDefinition.density = 0;
@@ -196,7 +243,8 @@ edge.m_vertex3 = v3;*/
 		body.createFixture (fixtureDefinition);
 
 	}
-	private function this_onEnterFrame (event:Event):Void {
+	
+	private function this_onEnterFrame (e:Event):Void {
 		
 		//////
 		//backButton.x = bodyTest.getPosition().x/ PHYSICS_SCALE-backButton.width/2;
@@ -206,5 +254,57 @@ edge.m_vertex3 = v3;*/
 		World.step (1 / 30, 10, 10);
 		World.clearForces ();
 		World.drawDebugData ();
+		
+		///////////
+		if (mouseJoint != null) {
+			var p2:B2Vec2=new B2Vec2(mouseXWorldPhys,mouseYWorldPhys);
+			mouseJoint.setTarget(p2);
+		}
 	}
+		public function GetBodyAtMouse(includeStatic:Bool=false,e:MouseEvent):B2Body {
+			var mouseXWorldPhys:Float = (e.stageX)*PHYSICS_SCALE;
+			var mouseYWorldPhys:Float = (e.stageY)*PHYSICS_SCALE;
+			var mousePVec:B2Vec2 = new B2Vec2(mouseXWorldPhys, mouseYWorldPhys);
+			
+			var aabb:B2AABB = new B2AABB();
+			aabb.lowerBound.set(mouseXWorldPhys - 0.001, mouseYWorldPhys - 0.001);
+			aabb.upperBound.set(mouseXWorldPhys + 0.001, mouseYWorldPhys + 0.001);
+			var k_maxCount:Int=10;
+			//var shapes:Array = new Array();
+			//var count:Int =
+			World.queryAABB(callBackQueryAABB,aabb);
+			var body:B2Body=null;
+			/*for (var i:int = 0; i < count; ++i) {
+				if (shapes[i].GetBody().IsStatic()==false||includeStatic) {
+					var tShape:b2Shape=shapes[i] as b2Shape;
+					var inside:Boolean=tShape.TestPoint(tShape.GetBody().GetXForm(),mousePVec);
+					if (inside) {
+						body=tShape.GetBody();
+						break;
+					}
+				}
+			}*/
+			return body;
+		}
+		public var queriedBody:B2Body;
+		public function callBackQueryAABB(fixture:B2Fixture):Bool {
+			trace(fixture);
+			queriedBody = fixture.getBody();
+			return fixture!=null;
+		}
+		
+		public var mouseXWorldPhys:Float;
+		public var mouseYWorldPhys:Float;
+		
+		public function onMouseMove(e:MouseEvent) {
+			mouseXWorldPhys=e.stageX*PHYSICS_SCALE;
+			mouseYWorldPhys=e.stageY*PHYSICS_SCALE;
+		}
+		public function onMouseDown(e:MouseEvent) {
+			if (mouseJoint!=null) {
+				World.destroyJoint(mouseJoint);
+				mouseJoint = null;
+				trace("onMouseDown");
+			}
+		}
 }
